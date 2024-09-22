74f91a161805089e5b94937061e36d3f5e1e29d1
Bohdan Poliienko <mignatov756@gmail.com>
2024-09-22 20:22:48 +0300

    fixed flakes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -7,7 +7,7 @@ class Car:
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float, clean_power: int,
-                 average_rating: float, count_of_ratings: int):
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -32,5 +32,5 @@ class CarWashStation:
 
     def rate_service(self, rate: int) -> None:
         self.count_of_ratings += 1
-        self.average_rating = round(
-            (self.average_rating * (self.count_of_ratings - 1) + rate) / self.count_of_ratings, 1)
+        self.average_rating = round((self.average_rating
+                                     * (self.count_of_ratings - 1) + rate) / self.count_of_ratings, 1)
